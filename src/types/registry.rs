b72d7d8108b93973b390a9d973723dffec5b9186
deniz.hofmeister <deniz.hofmeister@gmail.com>
2024-09-20 16:32:59 +0200

    added todos

--- src/types/registry.rs
+++ src/types/registry.rs
@@ -1,5 +1,5 @@
 use crate::types::{Buffer, Timestamp, Transform};
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 
 pub struct Registry {
     pub data: HashMap<String, Buffer>,
@@ -27,9 +27,35 @@ impl Registry {
     pub fn get_transform<'a>(
         &mut self,
         from: &'a str,
-        _to: &'a str,
+        to: &'a str,
         timestamp: Timestamp,
     ) -> Option<Transform> {
-        self.data.get(from)?.get(&timestamp)
+        //TODO: Finish this.
+        // 1. Iterate upwards the transform tree until parents don't exist
+        // 2. find out if both the to-tree and the from-tree mention the same parents anywhere in
+        //    the list.
+        // 2b. if not, find out if the final parent, is the same parent, if so, create parent
+        // 2c. if not, exit
+        // 3. Assemble transforms
+        let mut from_iterator = Some(from.clone());
+        let mut to_iterator = Some(to.clone());
+        let mut from_transforms_vec = VecDeque::<Transform>::new();
+        let mut to_transforms_vec = VecDeque::<Transform>::new();
+
+        loop {
+            let buffer = self.data.get(from);
+            if buffer.is_none() {
+                break;
+            };
+
+            let transf = buffer.unwrap().get(&timestamp);
+            if transf.is_none() {
+                break;
+            }
+
+            from_transforms_vec.push_back(transf.unwrap());
+        }
+
+        None
     }
 }
